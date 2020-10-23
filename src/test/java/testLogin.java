66cf6dae6d895be2b4357596c91711cda5497c43
moa114 <57449717+moa114@users.noreply.github.com>
2020-10-23 11:29:14 +0200

    header comment

--- src/test/java/testLogin.java
+++ src/test/java/testLogin.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
+/**
+ * @author Christian Lind
+ */
 public class testLogin {
 
     @Test
